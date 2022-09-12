f611ff7ff13002e069556708f945ef36a8b53c3f
Artem Chebotko <artemchebotko@gmail.com>
2022-09-12 10:14:48 -0500

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -16,11 +16,15 @@ echo ""
 #echo "Choose Astra DB or Cassandra on the left panel to start."
 
 echo -n 'Starting a Cassandra cluster ...'; 
-timeout 60 bash -c 'until cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 2; echo -n "."; done'; 
+timeout 60 bash -c 'until cqlsh localhost 9042 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
+timeout 60 bash -c 'until cqlsh localhost 9043 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
+sleep 2
 echo ' DONE!'
 echo "Cassandra successfully started."
 
 echo ""
+
+sleep 2
 
 
 
