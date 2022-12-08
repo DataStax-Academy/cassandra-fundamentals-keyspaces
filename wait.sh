a0fb0811fd9fc58af6fb91f60817afed5de27243
Artem Chebotko <artemchebotko@gmail.com>
2022-12-08 14:00:29 -0600

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -17,9 +17,10 @@ echo ""
 
 echo -n 'Starting a Cassandra cluster ...'; 
 timeout 60 bash -c 'until cqlsh localhost 9042 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
-sleep 2
+#sleep 2
 timeout 60 bash -c 'until cqlsh localhost 9043 -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
-sleep 2
+#sleep 2
+timeout 60 bash -c 'dc_count=0; until [ $dc_count -eq 2 ] >/dev/null 2>&1; do sleep 1; dc_count=$( docker exec -i -t Cassandra-1 bash -c "nodetool status" | grep -e "Datacenter" -c ); echo -n "."; done'
 echo ' DONE!'
 echo "Cassandra successfully started."
 
